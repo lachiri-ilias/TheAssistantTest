7aad697a86803d5a004de8e5ffdd28499370d730
LACHIRI <lachiri@kth.se>
2024-04-09 03:24:19 +0200

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-3TJ6OD7S.mjs";import"./chunk-OOJZHL5J.mjs";import"./chunk-7OXAB4D3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-76T5DRAI.mjs";import"./chunk-OOJZHL5J.mjs";import"./chunk-7OXAB4D3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
